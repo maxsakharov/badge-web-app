8864865f2845063f20c52cc49be85c37d2781057
nhryshchankova <nhryshchankova@edmunds.com>
2017-11-20 22:51:41 -0800

    Initial app setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Manage your badges!</h1>
+        </header>
+        <p className="App-intro">
+          Some awesome content goes here
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
