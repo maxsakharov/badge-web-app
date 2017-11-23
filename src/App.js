15f98f6e812b8bab652e57efad9597cea59e840c
Andrei Daniliuk <adaniliuk@edmunds.com>
2017-11-22 18:13:17 -0800

    Added top bar and bottom nav components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import Button from 'material-ui/Button';
 import { showBadgeUploadForm } from './actions/actions';
 import BadgeUploadForm from './components/badge-upload-form'
+import AppBar from './components/app-top-bar';
+import BottomNavigation from './components/app-bottom-navigation';
 import 'typeface-roboto';
 import './App.css';
 
@@ -10,13 +13,17 @@ class AppUI extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Manage your badges</h1>
-        </header>
-        <div className="navigation">
-          <button type="button" className="button" onClick={this.props.openBadgeUploadForm}>Upload badge</button>
+        <AppBar />
+        <div style={{ top: '70px', bottom: '70px', position: 'fixed', width: '100%', overflowY: 'scroll' }}>
+          <header className="App-header">
+            <h1 className="App-title">Manage your badges</h1>
+          </header>
+          <div className="navigation">
+            <Button raised color="primary" onClick={this.props.openBadgeUploadForm}>Upload badge</Button>
+          </div>
+          <BadgeUploadForm />
         </div>
-        <BadgeUploadForm />
+        <BottomNavigation />
       </div>
     );
   }
---
+++ src/components/app-top-bar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  flex: {
+    flex: 1,
+  },  
+});
+
+function AppTopBar(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" color="default">
+        <Toolbar>
+          <Typography type="title" color="inherit" className={classes.flex}>
+            Parking Concierge
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+AppTopBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AppTopBar);
