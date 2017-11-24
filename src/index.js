f56d120107372ff63c267a100019fc17df763642
Andrei Daniliuk <adaniliuk@edmunds.com>
2017-11-23 21:08:06 -0800

    Added redux-thunk redux middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import badgesApp from './reducers/reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-const store = createStore(badgesApp, {});
+const storeEnhancer = applyMiddleware(thunk);
+const store = createStore(badgesApp, storeEnhancer);
+
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 registerServiceWorker();
