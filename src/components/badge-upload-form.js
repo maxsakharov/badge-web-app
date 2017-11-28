b085dd0872f3367ecdd6d0ebb9547c011ba5c2fa
Andrei Daniliuk <adaniliuk@edmunds.com>
2017-11-27 20:15:51 -0800

    Working badge upload on desktop

--- src/components/badge-upload-form.js
+++ src/components/badge-upload-form.js
@@ -1,8 +1,12 @@
+/* eslint-disable no-nested-ternary */
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'material-ui/Button';
 import Camera from 'material-ui-icons/Camera';
 import FileUpload from 'material-ui-icons/FileUpload';
+import Autorenew from 'material-ui-icons/Autorenew';
+import Done from 'material-ui-icons/Done';
 import { withStyles } from 'material-ui/styles';
 
 const styles = theme => ({
@@ -30,36 +34,23 @@ class BadgeUploadForm extends Component {
     classes: PropTypes.object.isRequired,
   }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      file: null,
-    };
+  state = {
+    file: null,
+    fileSrc: null,
+    uploading: false,
+    uploaded: false,
   }
 
-  selectFile(e) {
-    e.preventDefault();
-
-    const reader = new FileReader();
-    const file = e.target.files && e.target.files[0];
-    if (file) {
-      reader.onloadend = upload => this.setState({
-        file: upload.target.result,
-      }, this.uploadFile);
-      reader.readAsDataURL(file);
-    }
-  }
-
-  uploadFile() {
+  getCurrentPositionSuccess = (position) => {
+    const { latitude, longitude } = position.coords;
     const formData = new FormData();
     formData.append('badge', this.state.file);
-    fetch('http://localhost/badge/', {
+    formData.append('name', 'current parking permit');
+    formData.append('lat', latitude);
+    formData.append('long', longitude);
+
+    fetch('http://52.4.240.117:8080/badge', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'multipart/form-data',
-        Accept: 'application/json',
-        type: 'formData',
-      },
       body: formData,
     }).then((response) => {
       // TODO proper response handling - display badge info etc.
@@ -67,43 +58,100 @@ class BadgeUploadForm extends Component {
     }).catch(() => {
       // TODO proper handling
       console.log('Error');
+    }).then(() => {
+      this.setState({
+        uploading: false,
+        uploaded: true,
+      });
     });
+  }
+
+  error = error => console.log(error);
+
+  uploadFile = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      uploading: true,
+    }, () => {
+      navigator.geolocation.getCurrentPosition(this.getCurrentPositionSuccess, this.error);
+    });
+  }
+
+  selectFile = (e) => {
+    e.preventDefault();
+
+    const reader = new FileReader();
+    const file = e.target.files && e.target.files[0];
+    if (file) {
+      reader.onloadend = upload => this.setState({
+        file,
+        fileSrc: upload.target.result,
+      });
+      reader.readAsDataURL(file);
+    }
   }
 
   render() {
     const { classes } = this.props;
-    const { file } = this.state;
+    const { fileSrc, uploading, uploaded } = this.state;
 
     return (
       <div className={classes.badgeUploadForm}>
-        <Button
-          className={classes.button}
-          raised
-          id="raised-button-file"
-          component="label"
-          color="primary"
-          label="Ticket"
-        >
-          {file
-            ? [
-              <span key="label">Upload Ticket</span>,
-              <FileUpload key="icon" className={classes.rightIcon} />,
-            ]
-            : [
-              <span key="label">Select Ticket</span>,
-              <Camera key="icon" className={classes.rightIcon} />,
-              <input
-                key="file"
-                accept="image/*"
-                type="file"
-                style={{ display: 'none' }}
-                onChange={e => this.selectFile(e)}
-              />,
-            ]
-          }
-        </Button>
-
-        <img className={classes.imagePreview} src={file} />
+        {uploaded
+          ? (
+            <Button
+              className={classes.button}
+              raised
+              id="raised-button-file"
+              component="label"
+              color="primary"
+              label="Ticket"
+              onClick={fileSrc ? this.uploadFile : () => {}}
+            >
+              {[
+                <span key="label">Uploaded</span>,
+                <Done key="icon" className={classes.rightIcon} />,
+              ]}
+            </Button>
+          )
+          : (
+            <Button
+              className={classes.button}
+              raised
+              id="raised-button-file"
+              component="label"
+              color="primary"
+              label="Ticket"
+              onClick={fileSrc ? this.uploadFile : () => {}}
+            >
+              {fileSrc
+                ?
+                  uploading
+                    ? [
+                      <span key="label">Uploading...</span>,
+                      <Autorenew key="icon" className={classes.rightIcon} />,
+                    ]
+                    : [
+                      <span key="label">Upload Ticket</span>,
+                      <FileUpload key="icon" className={classes.rightIcon} />,
+                    ]
+                : [
+                  <span key="label">Select Ticket</span>,
+                  <Camera key="icon" className={classes.rightIcon} />,
+                  <input
+                    key="file"
+                    accept="image/*"
+                    type="file"
+                    style={{ display: 'none' }}
+                    onChange={e => this.selectFile(e)}
+                  />,
+                ]
+              }
+            </Button>
+          )
+        }
+        <img className={classes.imagePreview} src={fileSrc} />
       </div>
     );
   }
